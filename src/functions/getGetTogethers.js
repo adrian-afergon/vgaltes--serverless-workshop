0ac4499ec1b7cea11e06351687d299a6594c99de
Adrian Ferrera <adrian.afergon@gmail.com>
2020-02-14 14:37:19 +0000

    Get real data from DynamoDB

---
+++ src/functions/getGetTogethers.js
@@ -0,0 +1,23 @@
+const AWS = require("aws-sdk");
+
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const tableName = 'gettogethers';
+
+module.exports.handler = async (event) => {
+
+    const count = 8;
+
+    const req = {
+        TableName: tableName,
+        Limit: count
+    };
+
+    const resp = await dynamodb.scan(req).promise();
+
+    const res = {
+        statusCode: 200,
+        body: JSON.stringify(resp.Items)
+    };
+
+    return res;
+};
